460c99193915b4109fd460e4798e19ccbbd31152
Manavi Lahoti <manavilahoti07@gmail.com>
2023-12-28 23:36:59 +0530

    connected database

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="mysql://username:password@server/db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
