faae707d415d926490924b2ac836fa99cb137cd5
Manavi Lahoti <manavilahoti07@gmail.com>
2024-01-14 20:47:29 +0530

    database

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']="mysql://username:password@server/db"
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.app_context().push()
 db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200),nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default= datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
 
 @app.route("/")
 def hello_world():
+    todo = Todo(title="first todo", desc="first task")
+    db.session.add(todo)
+    db.session.commit()
     css_url=url_for('static', filename='style.css')
     return render_template('index.html', css_url=css_url)
 
